043cd2e1204f77757579059155108ed7517f3261
happyyuwei1994 <happyyuwei1994@qq.com>
2020-02-22 19:10:11 +0800

    0.1.3
    1. 修复若干bug
    2. 增加隐藏线索功能。
    3. 改善用户体验。

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ let dom = window.document
 let timeline = [];
 /**
  * 时间线数据结构
-timeline:[
-    {
+ timeline:[
+ {
         "time":20000,   //时间存储的是当天0点开始的分钟数。
-        "detailArray":[    //一个时间下面会存在多个人的行动细节
+        "nameArray":[    //一个时间下面会存在多个人的行动细节
             {
                 "name":"" ,    //该细节的主角，
-                "detail":[   //详细信息以字符串保存
+                "detailArray":[   //详细信息以字符串保存
                     "detail information", ...
                 ],
-                "visible":[   //设置细节是否可见
+                "visibleArray":[   //设置细节是否可见
                      true,
                      false,
                      ...
@@ -23,8 +23,8 @@ timeline:[
             ...
         ]
     },
-    ...
-]
+ ...
+ ]
  */
 
 //角色, 记录角色的基本信息用于渲染
@@ -39,8 +39,46 @@ let id_detail_map = {};
 let name_input = dom.getElementById("name_input");
 let time_input = dom.getElementById("time_input");
 let detail_input = dom.getElementById("detail_input");
-let warning_div = dom.getElementById("warning_div")
+let warning_div = dom.getElementById("warning_div");
+//记住 选上是不可见
+let invisible_checkbox = dom.getElementById("invisible_checkbox");
+//开启将隐藏不可见细节
+let invisible_render_switch = dom.getElementById("invisible_render_switch");
 
+//是否处于编辑模式。在处于编辑模式下，可以直接修改时间线，但不能编辑左侧段落
+//在非编辑模式下，id为null
+let is_editable_mode = false;
+let editing_id = null;
+
+
+/**
+ * 编辑模式下，左侧栏不能跟新，冻结输入框
+ */
+let editingMode = () => {
+    if(is_editable_mode===false) {
+        name_input.setAttribute("readonly", "readonly");
+        time_input.setAttribute("readonly", "readonly");
+        detail_input.setAttribute("readonly", "readonly");
+        is_editable_mode = true;
+    }
+};
+
+/**
+ * 普通模式
+ */
+let normalMode = () => {
+
+    if(is_editable_mode===true) {
+        //清空面板
+        clear_input();
+
+        name_input.removeAttribute("readonly");
+        time_input.removeAttribute("readonly");
+        detail_input.removeAttribute("readonly");
+        is_editable_mode = false;
+        editing_id = null;
+    }
+};
 
 /**
  * 保存时间线至本地
@@ -73,6 +111,15 @@ let load = () => {
         return false;
     }
 };
+
+let clear_input = () => {
+    time_input.value = "";
+    detail_input.value = "";
+    invisible_checkbox.checked = false;
+    //聚焦在时间输入框
+    time_input.focus();
+    time_input.select();
+};
 /**
  * 清空过去的时间线
  */
@@ -87,8 +134,8 @@ let clear = () => {
 
     //刷新面板
     name_input.value = "";
-    time_input.value = "";
-    detail_input.value = "";
+    clear_input();
+
 };
 
 /**
@@ -130,6 +177,11 @@ let checkTime = (time_str) => {
     if (str_array.length === 1) {
         return false;
     }
+
+    if (time_str.includes("NaN")) {
+        return false
+    }
+
     let hour = parseInt(str_array[0]);
     let minute = parseInt(str_array[1]);
 
@@ -145,7 +197,7 @@ let checkTime = (time_str) => {
  * @param time
  * @returns {*}
  */
-let searchTimeline = (time) => {
+let searchInTimeline = (time) => {
     for (let i = 0; i < timeline.length; i++) {
         if (timeline[i].time === time) {
             return timeline[i];
@@ -161,7 +213,7 @@ let searchTimeline = (time) => {
  * @param name
  * @returns {*}
  */
-let searchName = (nameArray, name) => {
+let searchInNameArray = (nameArray, name) => {
     for (let i = 0; i < nameArray.length; i++) {
         if (nameArray[i].name === name) {
             return nameArray[i];
@@ -170,8 +222,8 @@ let searchName = (nameArray, name) => {
     return null;
 };
 
-let checkInput = () => {
 
+let checkInput = () => {
     //去除空格
     name_input.value = name_input.value.replace(/ /g, "");
     time_input.value = time_input.value.replace(/ /g, "");
@@ -220,17 +272,20 @@ let updateTimeline = () => {
 
     //添加至时间线
     let time = parseTime(time_input.value);
-    let point = searchTimeline(time);
+    let point = searchInTimeline(time);
     //该时间已经存在
     if (point !== null) {
-        let name_detail = searchName(point.detailArray, name_input.value);
+        let name_detail = searchInNameArray(point.nameArray, name_input.value);
+        //该用户有记录
         if (name_detail != null) {
-            name_detail.detail = detail_input.value;
+            name_detail.detailArray.push(detail_input.value);
         } else {
-            point.detailArray.push(
+            //该用户无记录
+            point.nameArray.push(
                 {
                     "name": name_input.value,
-                    "detail": detail_input.value
+                    "detailArray": [detail_input.value],
+                    "visibleArray": [!invisible_checkbox.checked]
                 }
             )
         }
@@ -238,10 +293,11 @@ let updateTimeline = () => {
         //时间点不存在则新增
         let point = {
             "time": time,
-            "detailArray": [
+            "nameArray": [
                 {
                     "name": name_input.value,
-                    "detail": detail_input.value
+                    "detailArray": [detail_input.value],
+                    "visibleArray": [!invisible_checkbox.checked]
                 }
             ]
         };
@@ -258,15 +314,14 @@ let updateTimeline = () => {
     });
 
     //刷新面板
-    time_input.value = "";
-    detail_input.value = "";
+    clear_input();
 
-    // console.log(timeline);
+    console.log(timeline);
 
-    //绘制时间线
+    // 绘制时间线
     visualTimeline();
 
-    //保存时间线
+    // 保存时间线
     save();
 
 };
@@ -295,80 +350,126 @@ let renderDetail = (detail) => {
     return detail;
 };
 
+
 /**
  * 创建一个时间点中的一个人的细节
- * @param time
- * @param nameDetail
+ * @param time_str
+ * @param nameArray
  */
-let createNameDetail = (time, nameDetail) => {
+let createNameArray = (time_str, nameArray) => {
 
     let div = dom.createElement("div");
 
-    for (let i = 0; i < nameDetail.length; i++) {
-        let p = dom.createElement("p");
-        //添加样式
-        // p.setAttribute("style","padding: 100px")
+    for (let i = 0; i < nameArray.length; i++) {
 
-        //名称
-        let name_span = dom.createElement("span");
-        //添加样式
-        name_span.setAttribute("class", "name");
-        name_span.innerText = nameDetail[i].name;
+        //查看一个名字下是否全是不可见，如果是，则名字不必渲染。
+        let bool = nameArray[i].visibleArray.reduce((total, current) => {
+            return total || current;
+        });
 
-        //分割
-        let split_span = dom.createElement("span");
-        name_span.setAttribute("class", "name");
-        split_span.innerText = "：";
+        if (invisible_render_switch.checked === false || bool === true) {
 
-        //细节
-        let detail_span = dom.createElement("span");
-        // detail_span.innerText = nameDetail[i].detail;
-        detail_span.innerHTML = renderDetail(nameDetail[i].detail);
-        // detail_span.innerHTML=nameDetail[i].detail;
-        // 创建ID
-        let detail_span_id = "detail_" + Math.random();
-        detail_span.setAttribute("id", detail_span_id);
-        detail_span.setAttribute("contenteditable", "true")
-        //记录ID
-        id_detail_map[detail_span_id] = {
-            "time": time,
-            "name": nameDetail[i].name,
-            "detail": nameDetail[i].detail
-        };
+            let p = dom.createElement("p");
+            //添加样式
+            // p.setAttribute("style","padding: 100px")
 
-        //添加按键事件，按键完成将会在输入面板显示
-        detail_span.addEventListener("click", (e) => {
-            let detail_click = id_detail_map[e.target.id];
-            // console.log(e);
-            if (detail_click === undefined) {
-                detail_click=id_detail_map[e.path[1].id];
+            //名称
+            let name_span = dom.createElement("span");
+            //添加样式
+            name_span.setAttribute("class", "name");
+            name_span.innerText = nameArray[i].name;
+
+            //分割
+            let split_span = dom.createElement("span");
+            name_span.setAttribute("class", "name");
+            split_span.innerText = "：";
+
+            //添加组件
+            p.appendChild(name_span);
+            p.appendChild(split_span);
+
+            //===============================================================================
+            //细节
+            for (let j = 0; j < nameArray[i].detailArray.length; j++) {
+
+                if (nameArray[i].visibleArray[j] === true || invisible_render_switch.checked === false) {
+
+                    let detail_span = dom.createElement("span");
+                    detail_span.innerHTML = renderDetail(nameArray[i].detailArray[j]);
+
+                    // 创建ID
+                    let detail_span_id = "detail_" + Math.random();
+                    detail_span.setAttribute("id", detail_span_id);
+                    detail_span.setAttribute("contenteditable", "true");
+                    detail_span.setAttribute("class", "edit-paragraph");
+
+                    //记录ID
+                    id_detail_map[detail_span_id] = {
+                        "time": time_str,
+                        "name": nameArray[i].name,
+                        "detail": nameArray[i].detailArray[j],
+                        "id": j,    //id为对应detailArray中的第几条细节。
+                        "visible": nameArray[i].visibleArray[j],
+                    };
+
+                    //修改完成失去焦距即重新渲染
+                    detail_span.addEventListener("blur", (e) => {
+                        //修改时当场渲染
+                        //添加
+                        e.target.innerHTML = renderDetail(e.target.innerText);
+                    });
+
+                    detail_span.addEventListener("focus", (e) => {
+                        //获取修改权限，冻结左侧窗口。
+                        editingMode();
+                        //记录编辑模式下允许编辑的组件
+                        editing_id = e.target.id;
+                    });
+
+                    //添加按键事件，按键完成将会在输入面板显示
+                    detail_span.addEventListener("click", (e) => {
+                        let detail_click = id_detail_map[e.target.id];
+                        // console.log(e);
+                        if (detail_click === undefined) {
+                            detail_click = id_detail_map[e.path[1].id];
+                        }
+                        // console.log(detail_click);
+                        name_input.value = detail_click.name;
+                        time_input.value = detail_click.time;
+                        detail_input.value = detail_click.detail;
+                        // console.log(detail_click.visible);
+                        invisible_checkbox.checked = !detail_click.visible;
+                    });
+                    //时间线上可直接修改
+                    detail_span.addEventListener("keyup", (e) => {
+                        // let modify = dom.getElementById(e.target.id).innerText;
+                        let modify = e.target.innerText;
+                        //获取修改的内容
+                        //面板上同步修改
+                        detail_input.value = modify;
+
+                        //在时间线上修改的内容可直接保存，无需任何按键
+                        //先获取所触发的时间-姓名-细节
+                        let detail_click = id_detail_map[e.target.id];
+                        //寻找保存到具体的内存中
+                        searchInNameArray(searchInTimeline(parseTime(detail_click.time)).nameArray, detail_click.name).detailArray[detail_click.id] = modify;
+
+                        //保存到映射缓存中
+                        detail_click.detail = modify;
+
+                        //保存时间线
+                        save();
+
+                    });
+                    //追加细节
+                    p.appendChild(detail_span);
+                }
             }
-            // console.log(detail_click);
-            name_input.value = detail_click.name;
-            time_input.value = detail_click.time;
-            detail_input.value = detail_click.detail;
-        });
-        //时间线上可直接修改
-        detail_span.addEventListener("keyup", (e) => {
-            let modify = dom.getElementById(e.target.id).innerText;
-            //获取修改的内容
-            //面板上同步修改
-            detail_input.value = modify;
 
-            //在时间线上修改的内容可直接保存，无需任何按键
-            //先获取所触发的时间-姓名-细节
-            let detail_click = id_detail_map[e.target.id];
-            //寻找保存到具体的内存中
-            searchName(searchTimeline(parseTime(detail_click.time)).detailArray, detail_click.name).detail = modify;
-            //保存时间线
-            save();
-        });
 
-        //添加组件
-        p.appendChild(name_span);
-        p.appendChild(split_span);
-        p.appendChild(detail_span);
-        div.appendChild(p)
+            //追加段落
+            div.appendChild(p)
+        }
     }
 
     return div
@@ -396,7 +497,7 @@ let createTimePoint = (timePoint) => {
     let time_str = formatTime(timePoint.time);
     time_h.innerText = time_str;
 
-    let detail_p = createNameDetail(time_str, timePoint.detailArray);
+    let detail_p = createNameArray(time_str, timePoint.nameArray);
 
     div.appendChild(time_h);
     div.appendChild(detail_p);
@@ -406,6 +507,24 @@ let createTimePoint = (timePoint) => {
 
     return list;
 };
+
+/**
+ * 检查时间点里是否全是不可见
+ * @param timePoint
+ */
+let checkInvisibleInTimePoint = (timePoint) => {
+
+    for (let i = 0; i < timePoint.nameArray.length; i++) {
+        let bool = timePoint.nameArray[i].visibleArray.reduce((total, current) => {
+            return total || current;
+        });
+        if (bool === true) {
+            return true
+        }
+    }
+    return false;
+};
+
 /**
  * 绘制时间线
  */
@@ -428,8 +547,13 @@ let visualTimeline = () => {
     timeline_ul.setAttribute("id", "timeline");
     //时间线
     for (let i = 0; i < timeline.length; i++) {
-        let timepoint = createTimePoint(timeline[i]);
-        timeline_ul.appendChild(timepoint);
+        let timePoint = createTimePoint(timeline[i]);
+
+        //检查是否全为不可见，如果全不可见，则该时间线不显示。
+        let bool = checkInvisibleInTimePoint(timeline[i]);
+        if (invisible_render_switch.checked === false || bool === true) {
+            timeline_ul.appendChild(timePoint);
+        }
     }
     timeline_container.appendChild(timeline_ul);
 };
@@ -441,18 +565,24 @@ let start = () => {
 
     //添加点击事件更新时间线
     let update_button = dom.getElementById("update_button");
-    update_button.addEventListener("click", updateTimeline);
+    update_button.addEventListener("click", () => {
+        if (is_editable_mode === false) {
+            updateTimeline();
+        }
+    });
 
     //添加快捷键事件更新时间线
     document.addEventListener("keydown", (e) => {
         // 快捷键是alt+s
         if (e.altKey === true && e.key === "s") {
             //快捷键刷新时间线
-            updateTimeline()
+            //编辑模式下会自动刷新，不允许手动刷新
+            if (is_editable_mode === false) {
+                updateTimeline();
+            }
         } else if (e.altKey === true && e.key === "c") {
             //快捷键清空输入面板
-            time_input.value = "";
-            detail_input.value = "";
+            clear_input();
         }
     });
 
@@ -495,6 +625,34 @@ let start = () => {
     dom.getElementById("help").addEventListener("click", () => {
         alert("清除输入框：alt+c，更新时间线：alt+s。可以直接在生成的时间线上修改内容。");
     });
+
+    //添加渲染选择事件
+    dom.getElementById("invisible_render_switch_div").addEventListener("click", () => {
+        //重新绘制时间线
+        visualTimeline();
+        //刷新面板
+        clear_input();
+    });
+
+    //添加编辑模式下，checkbox自动保存事件
+    invisible_checkbox.addEventListener("click", () => {
+
+        //在编辑模式下自动保存
+        if (editing_id !== null) {
+
+            //更新时间线
+            let detail_click = id_detail_map[editing_id];
+            searchInNameArray(searchInTimeline(parseTime(detail_click.time)).nameArray, detail_click.name).visibleArray[detail_click.id] = !invisible_checkbox.checked;
+
+            //跟新id缓存
+            detail_click.visible = !invisible_checkbox.checked;
+        }
+    });
+
+    //添加普通模式触发事件
+    name_input.addEventListener("click", normalMode);
+    time_input.addEventListener("click", normalMode);
+    detail_input.addEventListener("click", normalMode);
 
     //载入保存的时间线
     if (load()) {
